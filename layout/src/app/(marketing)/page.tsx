26f85312977ea339cc2631f1adcfb587bc994a4a
Muhammad Rehan Alam <rehan@beebolt.com>
2025-04-27 15:26:08 +0500

    adds meta data

---
+++ layout/src/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Welcome Home"
+}
+
+export default function Home() {
+    return <h1>Welcome to home!</h1>
+}
--- layout/src/app/(marketing)/product/page.tsx
+++ layout/src/app/(marketing)/product/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "Product Page"
+}
+
 export default function Product() {
     return <h1>Product Page</h1>
 }
