0d0fa5228573aefc82e507be6e93bc5ed1ea37e4
Muhammad Rehan Alam <rehan@beebolt.com>
2025-04-27 16:12:24 +0500

    chore: Adding metadata for client pages

---
+++ layout/src/app/(marketing)/counter/page.tsx
@@ -0,0 +1,12 @@
+import Counter from "./counter";
+
+export const metadata = {
+    title: 'Counter'
+}
+
+export default function CounterPage() {
+
+    return <div>
+      <Counter/>
+    </div>
+}
